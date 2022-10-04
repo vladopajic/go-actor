34e7c104e6e9e1e0218bae8731bc7801ca3398be
Vlado Pajic <vlado@brrrrr.at>
2022-10-04 15:23:23 +0200

    context rework

---
+++ actor/context.go
@@ -0,0 +1,66 @@
+package actor
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+)
+
+// Context is provided so Worker can listen and respond on stop signal sent from Actor.
+type Context = context.Context
+
+var ErrStopped = errors.New("actor stopped")
+
+type contextImpl struct {
+	mu    sync.Mutex
+	err   error
+	doneC chan struct{}
+}
+
+func NewContext() *contextImpl { //nolint:revive
+	return &contextImpl{
+		doneC: make(chan struct{}),
+	}
+}
+
+func NewContextEnded() *contextImpl { //nolint:revive
+	doneC := make(chan struct{})
+	close(doneC)
+
+	return &contextImpl{
+		doneC: doneC,
+		err:   ErrStopped,
+	}
+}
+
+func (c *contextImpl) Deadline() (time.Time, bool) {
+	return time.Time{}, false
+}
+
+func (c *contextImpl) signalEnd() {
+	c.mu.Lock()
+	c.err = ErrStopped
+	close(c.doneC)
+	c.mu.Unlock()
+}
+
+func (c *contextImpl) Done() <-chan struct{} {
+	return c.doneC
+}
+
+func (c *contextImpl) Err() error {
+	c.mu.Lock()
+	err := c.err
+	c.mu.Unlock()
+
+	return err
+}
+
+func (*contextImpl) Value(key any) any {
+	return nil
+}
+
+func (c *contextImpl) String() string {
+	return "actor.Context"
+}
