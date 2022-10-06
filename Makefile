8344f0b2af4743db2b9df0679d8ec1bca9f07038
Vlado Pajic <vlado@brrrrr.at>
2022-10-06 14:06:16 +0200

    unit test improvements

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ lint: get-golangcilint
 # Runs tests on entire repo
 .PHONY: test
 test: 
-	go test --race -count=10 -failfast ./...
+	go test -timeout=3s -race -count=10 -failfast ./...
 
 # Code tidy
 .PHONY: tidy
--- actor/actor_test.go
+++ actor/actor_test.go
@@ -2,6 +2,7 @@ package actor_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -21,32 +22,106 @@ func Test_NewWorker(t *testing.T) {
 	w := NewWorker(workerFunc)
 	assert.NotNil(t, w)
 
-	// We assert that worker will delgate call to supplied workerFunc
-	assert.Equal(t, WorkerContinue, w.DoWork(ctx))
-	assert.Len(t, sigC, 1)
+	// We assert that worker will delegate call to supplied workerFunc
+	for i := 0; i < 10; i++ {
+		assert.Equal(t, WorkerContinue, w.DoWork(ctx))
+		assert.Len(t, sigC, 1)
+		<-sigC
+	}
 }
 
 func Test_NewActor(t *testing.T) {
 	t.Parallel()
 
-	w := newWorker()
+	w := newWorkerWithDoneC()
 	a := New(w)
 
 	a.Start()
-	defer a.Stop()
 
-	for i := 0; i < 20; i++ {
+	// Asset that worker is going to be executed by actor
+	for i := 0; i < 10; i++ {
 		p := make(chan int)
 		w.doWorkC <- p
 		assert.Equal(t, i, <-p)
 	}
+
+	a.Stop()
+
+	// Actor was stopped; wait for done signal
+	// and assert that worker is not running
+	<-w.doneC
+
+	p := make(chan int)
+	w.doWorkC <- p
+	select {
+	case <-p:
+		assert.FailNow(t, "actor should not be running worker")
+	case <-time.After(time.Millisecond * 20):
+	}
 }
 
-func Test_NewActor_StartStop(t *testing.T) {
+func Test_NewActor_OptOnStartStop(t *testing.T) {
+	t.Parallel()
+
+	onStartC := make(chan struct{}, 1)
+	onStopC := make(chan struct{}, 1)
+
+	a := New(newWorker(),
+		OptOnStart(func() { onStartC <- struct{}{} }),
+		OptOnStop(func() { onStopC <- struct{}{} }),
+	)
+
+	a.Start()
+	<-onStartC
+
+	a.Stop()
+	<-onStopC
+}
+
+func Test_NewActor_MultipleStartStop(t *testing.T) {
+	t.Parallel()
+
+	const count = 3
+
+	onStartC := make(chan struct{}, count)
+	onStopC := make(chan struct{}, count)
+
+	w := newWorkerWithDoneC()
+	a := New(w,
+		OptOnStart(func() { onStartC <- struct{}{} }),
+		OptOnStop(func() { onStopC <- struct{}{} }),
+	)
+
+	// Calling Start() multiple times should have same effect
+	// as calling it once
+	for i := 0; i < count; i++ {
+		a.Start()
+	}
+
+	for i := 0; i < 10; i++ {
+		p := make(chan int)
+		w.doWorkC <- p
+		assert.Equal(t, i, <-p)
+	}
+
+	// Calling Stop() multiple times should have same effect
+	// as calling it once
+	for i := 0; i < count; i++ {
+		a.Stop()
+	}
+
+	<-w.doneC
+	assert.Len(t, onStartC, 1)
+	assert.Len(t, onStopC, 1)
+}
+
+func Test_NewActor_Restart(t *testing.T) {
 	t.Parallel()
 
 	w := newWorker()
 	a := New(w)
+
+	// Assert that restarting actor will not impact worker execution
 
 	for i := 0; i < 20; i++ {
 		a.Start()
@@ -78,6 +153,7 @@ func Test_NewActor_StopAfterWorkerEnded(t *testing.T) {
 			return WorkerContinue
 
 		case <-c.Done():
+			// If we receive done signal from Actor we should fail test
 			assert.FailNow(t, "worker should be ended")
 			return WorkerEnd
 		}
@@ -99,24 +175,6 @@ func Test_NewActor_StopAfterWorkerEnded(t *testing.T) {
 
 	// Stoping actor should produce no effect (since worker has ended)
 	a.Stop()
-}
-
-func Test_NewActor_OptOnStartStop(t *testing.T) {
-	t.Parallel()
-
-	onStartC := make(chan struct{}, 1)
-	onStopC := make(chan struct{}, 1)
-
-	a := New(newWorker(),
-		OptOnStart(func() { onStartC <- struct{}{} }),
-		OptOnStop(func() { onStopC <- struct{}{} }),
-	)
-
-	a.Start()
-	<-onStartC
-
-	a.Stop()
-	<-onStopC
 }
 
 func Test_Combine_StartAll_StopAll(t *testing.T) {
@@ -161,12 +219,23 @@ func drain(c chan struct{}) {
 }
 
 func newWorker() *worker {
-	return &worker{doWorkC: make(chan chan int, 1)}
+	return &worker{
+		doWorkC: make(chan chan int, 1),
+		doneC:   nil,
+	}
+}
+
+func newWorkerWithDoneC() *worker {
+	return &worker{
+		doWorkC: make(chan chan int, 100),
+		doneC:   make(chan struct{}),
+	}
 }
 
 type worker struct {
 	workIteration int
 	doWorkC       chan chan int
+	doneC         chan struct{}
 }
 
 func (w *worker) DoWork(c Context) WorkerStatus {
@@ -182,6 +251,10 @@ func (w *worker) DoWork(c Context) WorkerStatus {
 		return WorkerContinue
 
 	case <-c.Done():
+		if w.doneC != nil {
+			close(w.doneC)
+		}
+
 		return WorkerEnd
 	}
 }
