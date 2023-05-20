128c0da271f5fa70afdadea3de364e9ab20ebdab
MarkGaskinRR <mark.g@roserocket.com>
2023-05-20 13:18:27 -0400

    [add_mail_server-add_mail_server] - Added supervisor and associated test

---
+++ actor/supervisor.go
@@ -0,0 +1,106 @@
+package actor
+
+import (
+	"fmt"
+	"sync"
+)
+
+type SupervisedWorker struct {
+	Worker
+	Inbox  <-chan Message
+	Outbox chan<- Message
+}
+
+func (sw *SupervisedWorker) GetInbox() <-chan Message {
+	return sw.Inbox
+}
+
+func (sw *SupervisedWorker) SetInbox(inbox <-chan Message) {
+	sw.Inbox = inbox
+}
+
+func (sw *SupervisedWorker) GetOutbox() chan<- Message {
+	return sw.Outbox
+}
+
+func (sw *SupervisedWorker) SetOutbox(outbox chan<- Message) {
+	sw.Outbox = outbox
+}
+
+type supervisedWorker interface {
+	Worker
+	GetInbox() <-chan Message
+	GetOutbox() chan<- Message
+	SetInbox(inbox <-chan Message)
+	SetOutbox(outbox chan<- Message)
+}
+
+type Supervisor struct {
+	actors   map[string]Actor
+	outboxes map[string]Mailbox[Message]
+	inboxes  map[string]Mailbox[Message]
+	IDs      []string
+	mu       sync.Mutex
+}
+
+type Message struct {
+	From    string
+	To      string
+	Type    int
+	Content string
+}
+
+func NewSupervisor() *Supervisor {
+	supervisor := Supervisor{
+		actors:   make(map[string]Actor),
+		outboxes: make(map[string]Mailbox[Message]),
+		inboxes:  make(map[string]Mailbox[Message]),
+	}
+	return &supervisor
+}
+
+func (s *Supervisor) RegisterWorker(worker supervisedWorker, ID string) {
+	inbox := NewMailbox[Message]()
+	outbox := NewMailbox[Message]()
+
+	worker.SetInbox(inbox.ReceiveC())
+	worker.SetOutbox(outbox.SendC())
+	actor := New(worker)
+	s.mu.Lock()
+	s.actors[ID] = Combine(actor, inbox, outbox)
+	s.outboxes[ID] = outbox
+	s.inboxes[ID] = inbox
+	s.mu.Unlock()
+	s.IDs = append(s.IDs, ID)
+}
+
+func (s *Supervisor) StartAll() {
+	for i := range s.IDs {
+		id := s.IDs[i]
+		actor := s.actors[id]
+		go func(id string) {
+			// Execute actor's work
+			fmt.Printf("Starting actor %s\n", id)
+			actor.Start()
+			// Listen for inbox
+			for message := range s.outboxes[id].ReceiveC() {
+				fmt.Printf("Supervisor received message of type %d from %s: %s\n", message.Type, message.From, message.Content)
+				s.mu.Lock()
+				inbox := s.inboxes[message.To]
+				s.mu.Unlock()
+
+				// Send message to worker
+				inbox.SendC() <- message
+			}
+		}(id)
+	}
+}
+
+func (s *Supervisor) StopAll() {
+	for _, id := range s.IDs {
+		s.mu.Lock()
+		actor := s.actors[id]
+		s.mu.Unlock()
+		actor.Stop()
+	}
+}
---
+++ actor/supervisor_test.go
@@ -0,0 +1,74 @@
+package actor
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type testConsumeWorker struct {
+	SupervisedWorker
+	ID string
+}
+
+func (tw *testConsumeWorker) DoWork(c Context) WorkerStatus {
+	// For simplicity, just wait for a message and then end
+	fmt.Print("Worker 2 waiting for message\n")
+	for msg := range tw.Inbox {
+		fmt.Printf("Worker 2 received message from %s: %s\n", msg.From, msg.Content)
+		return WorkerEnd
+	}
+
+	return WorkerEnd
+}
+
+type testProduceWorker struct {
+	SupervisedWorker
+	ID string
+}
+
+func (tw *testProduceWorker) DoWork(c Context) WorkerStatus {
+	// For simplicity, just send a message and then end
+	fmt.Print("testProduceWorker sending message\n")
+	tw.Outbox <- Message{
+		From:    tw.ID,
+		To:      "worker2",
+		Type:    0,
+		Content: "Hello from worker1",
+	}
+	return WorkerEnd
+
+}
+
+func TestSupervisor(t *testing.T) {
+	// Test creating a new Supervisor.
+	fmt.Printf("Creating supervisor\n")
+	worker1 := testProduceWorker{ID: "worker1"}
+	worker2 := testConsumeWorker{ID: "worker2"}
+	supervisor := NewSupervisor()
+	supervisor.RegisterWorker(&worker1, worker1.ID)
+	supervisor.RegisterWorker(&worker2, worker2.ID)
+
+	if _, ok := supervisor.actors["worker1"]; !ok {
+		t.Errorf("Expected worker1 to be in supervisor.actors")
+	}
+
+	if _, ok := supervisor.actors["worker2"]; !ok {
+		t.Errorf("Expected worker2 to be in supervisor.actors")
+	}
+
+	// Test starting and stopping all workers.
+	supervisor.StartAll()
+
+	// Allow some time for the goroutines to start.
+	time.Sleep(1 * time.Second)
+
+	for _, id := range supervisor.IDs {
+		_, ok := supervisor.actors[id]
+		if !ok {
+			t.Errorf("Expected actor %s to be running", id)
+		}
+	}
+
+	supervisor.StopAll()
+}
