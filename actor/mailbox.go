b22a9732aa305a9a2e144873a1761cdfc6e2e23a
Vlado Pajić <vlado@brrrrr.at>
2025-02-17 12:16:16 +0100

    add note

--- actor/mailbox.go
+++ actor/mailbox.go
@@ -231,6 +231,8 @@ func (m *mailbox[T]) Send(ctx Context, msg T) error {
 
 	select {
 	case <-m.stopSigC:
+		// this block can potentially not be covered with tests because of race condition.
+		// it can cause flakiness with this CI.
 		return fmt.Errorf("Mailbox.Send canceled: %w", ErrMailboxStopped)
 	case <-ctx.Done():
 		return fmt.Errorf("Mailbox.Send canceled: %w", ctx.Err())
