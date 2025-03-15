5aaed7a728b3950da7f13e2d39879f1a0c49ae40
vladopajic <vladopajic@users.noreply.github.com>
2025-03-15 10:20:26 +0100

    chore: improve actor benchmarks (#120)

--- actor/actor_benchmark_test.go
+++ actor/actor_benchmark_test.go
@@ -6,7 +6,7 @@ import (
 	. "github.com/vladopajic/go-actor/actor"
 )
 
-func BenchmarkActorProducerToConsumer(b *testing.B) {
+func BenchmarkActorProducerToConsumerMailbox(b *testing.B) {
 	produceNext := int(0)
 	doneC := make(chan struct{})
 
@@ -14,40 +14,97 @@ func BenchmarkActorProducerToConsumer(b *testing.B) {
 	// the best performance.
 	mbx := NewMailbox[any](OptAsChan(), OptCapacity(largeCap))
 
-	producer := New(NewWorker(func(c Context) WorkerStatus {
-		select {
-		case <-c.Done():
+	producer := New(NewWorker(func(ctx Context) WorkerStatus {
+		produceNext++
+		mbx.Send(ctx, produceNext) //nolint:errcheck // error should never happen
+
+		if b.N == produceNext {
 			return WorkerEnd
-
-		default:
-			produceNext++
-			mbx.Send(c, produceNext) //nolint:errcheck // error should never happen
-
-			if b.N == produceNext {
-				return WorkerEnd
-			}
-
-			return WorkerContinue
 		}
+
+		return WorkerContinue
 	}))
 
-	consumer := New(NewWorker(func(c Context) WorkerStatus {
-		select {
-		case <-c.Done():
+	consumer := New(NewWorker(func(Context) WorkerStatus {
+		rcv := <-mbx.ReceiveC()
+		if b.N == rcv {
+			close(doneC)
 			return WorkerEnd
-
-		case rcv := <-mbx.ReceiveC():
-			if b.N == rcv {
-				close(doneC)
-			}
-
-			return WorkerContinue
 		}
+
+		return WorkerContinue
 	}))
 
 	a := Combine(mbx, producer, consumer).Build()
 	a.Start()
 	defer a.Stop()
+
+	<-doneC
+}
+
+func BenchmarkActorProducerToConsumerChannel(b *testing.B) {
+	produceNext := int(0)
+	doneC := make(chan struct{})
+
+	c := make(chan any, largeCap)
+
+	producer := New(NewWorker(func(Context) WorkerStatus {
+		produceNext++
+		c <- produceNext
+
+		if b.N == produceNext {
+			return WorkerEnd
+		}
+
+		return WorkerContinue
+	}))
+
+	consumer := New(NewWorker(func(Context) WorkerStatus {
+		rcv := <-c
+		if b.N == rcv {
+			close(doneC)
+			return WorkerEnd
+		}
+
+		return WorkerContinue
+	}))
+
+	a := Combine(producer, consumer).Build()
+	a.Start()
+	defer a.Stop()
+
+	<-doneC
+}
+
+// This benchmark implements logic of ActorProducerToConsumer benchmark
+// using two goroutines and a channel.
+func BenchmarkNativeProducerToConsumer(b *testing.B) {
+	b.Helper()
+
+	produceNext := int(0)
+	doneC := make(chan struct{})
+	c := make(chan any, largeCap)
+
+	go func() { // producer
+		for {
+			produceNext++
+			c <- produceNext
+
+			if b.N == produceNext {
+				return
+			}
+		}
+	}()
+
+	go func() { // consumer
+		for {
+			rcv := <-c
+			if b.N == rcv {
+				close(doneC)
+				return
+			}
+		}
+	}()
 
 	<-doneC
 }
